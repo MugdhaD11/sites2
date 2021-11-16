4afbd06ad3ed17c6c43b0a5f999e7e0d8d3502d7
MugdhaD11 <93168425+MugdhaD11@users.noreply.github.com>
2021-11-15 21:30:27 -0800

    Add files via upload

---
+++ class 78/main.js
@@ -0,0 +1,5 @@
+var fruits = ["strawberries","apples", "bannanas", "blackberries"];
+console.log(fruits);
+console.log(fruits[1]);
+document.getElementById("display1").innerHTML = fruits;
+document.getElementById("first_element").innerHTML = fruits[0];
